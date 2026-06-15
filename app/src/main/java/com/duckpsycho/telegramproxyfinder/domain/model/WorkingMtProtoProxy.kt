5b744e1b5871376ac358946653ef1449ce4ddb77
reehalrohit <76120137+reehalrohit@users.noreply.github.com>
2026-06-15 08:28:20 +0530

    Country info Update WorkingMtProtoProxy.kt

--- app/src/main/java/com/duckpsycho/telegramproxyfinder/domain/model/WorkingMtProtoProxy.kt
+++ app/src/main/java/com/duckpsycho/telegramproxyfinder/domain/model/WorkingMtProtoProxy.kt
@@ -1,8 +1,9 @@
-package com.duckpsycho.telegramproxyfinder.domain.model
-
 data class WorkingMtProtoProxy(
     override val server: String,
     override val port: Int,
     override val secret: String,
     val pingMs: Long,
+
+    val country: String = "Unknown",
+    val countryCode: String = "--",
 ) : MtProtoProxyEndpoint
